f440c22b92e5a66d1b74fde337a78730932dcc8c
louis <louis.thievent@gmail.com>
2018-11-20 16:24:17 +0100

    DatePicker on

--- app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
+++ app/src/main/java/fr/wcs/blablawild/ItinerarySearchActivity.java
@@ -1,13 +1,19 @@
 package fr.wcs.blablawild;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.icu.util.Calendar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
@@ -23,6 +29,25 @@ public class ItinerarySearchActivity extends AppCompatActivity {
         final EditText dat             = findViewById(R.id.dateIn);
         final Button   toList          = findViewById(R.id.srcBt);
         final Intent   toItineraryList = new Intent(this, ItineraryListActivity.class);
+        final Calendar calendar        = Calendar.getInstance();
+
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                calendar.set(Calendar.YEAR, year);
+                calendar.set(Calendar.MONTH, month);
+                calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel(dat, calendar);
+            }
+        };
+
+
+        dat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(ItinerarySearchActivity.this, date, calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         toList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -40,4 +65,11 @@ public class ItinerarySearchActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void updateLabel(EditText dat, Calendar calendar) {
+        String           format        = "dd/MM/yy";
+        SimpleDateFormat sdf           = new SimpleDateFormat(format, Locale.CANADA);
+        dat.setText(sdf.format(calendar.getTime()));
+    }
+
 }
